9401cef4786b6f02f40b27ddab634185f03b30f7
Jiangwei Xu <xujiangwei81@gmail.com>
2015-09-21 11:29:04 +0800

    Modify chunk size 4KB

--- nucleus/src/net/cellcloud/talk/dialect/ChunkDialect.java
+++ nucleus/src/net/cellcloud/talk/dialect/ChunkDialect.java
@@ -43,7 +43,7 @@ import net.cellcloud.talk.stuff.SubjectStuff;
 public class ChunkDialect extends Dialect {
 
 	public final static String DIALECT_NAME = "ChunkDialect";
-	public final static int CHUNK_SIZE = 7168;
+	public static int CHUNK_SIZE = 4096;
 
 	protected boolean ack = false;
 
